21ced3fcf7d3544582ad51e35a01471faff21d9d
Jordan Minx <char6447@yahoo.com>
2018-05-09 14:25:35 -0500

    added view.js function

--- Frontend/view.js
+++ Frontend/view.js
@@ -31,6 +31,15 @@ var view = {
     pandl: function(profit) {
         console.log('P/L this run: ' + profit);
     }
+    
+    powerlogo: function() {
+        console.log(" oooooooo8                                       o8                oooooooo8    o8                           oooo                    ");
+        console.log("o888     88 oo oooooo   oooo   oooo ooooooooo   o888oo   ooooooo   888         o888oo   ooooooo     ooooooo    888ooooo    ooooooooo8"); 
+        console.log("888          888    888  888   888   888    888  888   888     888  888oooooo   888     ooooo888  888     888  888   888  888oooooo8 "); 
+        console.log("888o     oo  888          888 888    888    888  888   888     888         888  888   888    888  888          888   888  888        "); 
+        console.log(" 888oooo88  o888o           8888     888ooo88     888o   88ooo88   o88oooo888    888o  88ooo88 8o   88ooo888  o888o o888o   88oooo888"); 
+        console.log("                         o8o888     o888                                                                                             ");
+    }
 };
 
 module.exports = view;
